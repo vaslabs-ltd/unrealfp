3e71cdd53ef7bde28a944d292bc80c791404d768
Vasilis Nicolaou <vasilis.nicolaou@vaslabs.io>
2025-08-03 14:28:33 +0300

    Cleanups and script fixes

--- fab_package.sh
+++ fab_package.sh
@@ -1,18 +1,19 @@
-cd Plugins/UnrealFunctionalProgramming
+PLUGIN_NAME="UnrealFunctionalProgramming"
+cd Plugins/$PLUGIN_NAME
 directories_to_keep="Config/\
  Content/\
  Resources/\
  Source/\
- SteamInputSimplePlugin.uplugin"
+ $PLUGIN_NAME.uplugin"
 
 target_dir="../../target"
 
-zip -r -9 -X -q -o "SteamInputPlugin.zip" $directories_to_keep
+zip -r -9 -X -q -o "$PLUGIN_NAME.zip" $directories_to_keep
 
 [ -d $target_dir ] && rm -r $target_dir
 
 mkdir -p $target_dir/fab
 
-mv "SteamInputPlugin.zip" $target_dir/fab/SteamInputPlugin.zip
+mv "$PLUGIN_NAME.zip" $target_dir/fab/$PLUGIN_NAME.zip
 
 cd -
